26f421030819c829d3f7edcd46055cfc01444c8a
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-29 10:28:58 +0700

    ADD skills styles

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -1,18 +1,19 @@
 import { Fragment } from 'react'
+import styles from '../styles/SkillSection.module.css'
 
 const SkillSection = ({ heading, items }) => {
   const skillItem = items.map((text, index) => {
     return (
       <Fragment key={index}>
-        <h4>{text}</h4>
+        <h4 className={styles.skillItem}>{text}</h4>
       </Fragment>
     )
   })
 
   return (
-    <div>
-      <h2>{heading}</h2>
-      <div>{skillItem}</div>
+    <div className={styles.container}>
+      <h2 className={styles.skillHeading}>{heading}</h2>
+      <div className={styles.skillItemsContainer}>{skillItem}</div>
     </div>
   )
 }
